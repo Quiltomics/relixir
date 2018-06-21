b6b334a53f9b1aa0b42bd6f05095e60816386023
Trang Tran <ttdtrang@gmail.com>
2018-06-21 16:35:55 -0600

    some tests

--- test/relixir_test.exs
+++ test/relixir_test.exs
@@ -2,7 +2,44 @@ defmodule RelixirTest do
   use ExUnit.Case
   doctest Relixir
 
-  test "greets the world" do
-    assert Relixir.hello() == :world
+  test "single function call" do
+    assert Relixir.runR("cat('hello')") == "hello"
+    assert Relixir.runR("cat('sailing across\nthe oceans\n')") == """
+sailing across
+the oceans
+"""
   end
+
+  test "exporting variable" do
+    x = Relixir.runR("x <- (5+3)", "x")
+    y = Relixir.runR("y <- (2*4)", "y")
+    assert x == y
+  end
+
+  @tag :tentative
+  test "multiline R code" do
+    rCode =  """
+    a = 0
+    for (i in 1:10) {
+      a = a +i
+    }
+    a
+    """
+    assert Relixir.runR(rCode) == 55
+  end
+
+  @tag :tentative
+  test "datafile input" do
+  rCode = """
+  x = read.csv("some_url")
+  summary(x)
+  """
+  end
+
+  @tag :tentative
+  test "piping" do
+    Relixir.runR("matrix(1:10,nrow=2)")
+    |> Relixir.callRFunc("svd")
+  end
+
 end
